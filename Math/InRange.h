17881256e9a3340bb77a625783e4a3af5c91e41b
Thomas Schlott <thomas.schlott@outlook.de>
2022-09-03 20:53:10 +0200

    InRange: Added inRange check functions

---
+++ Math/InRange.h
@@ -0,0 +1,39 @@
+#ifndef THZ_COMMON_UTILITY_INRANGE_H
+#define THZ_COMMON_UTILITY_INRANGE_H
+
+#include <type_traits>
+
+namespace Terrahertz {
+
+/// @brief Checks if the given check is in the given range.
+///
+/// @tparam TType The type of the min, max and check.
+/// @param check The value to check.
+/// @param min The value smaller or equal to check.
+/// @param max The value greater or equal to check.
+/// @return True if check is within the range, false otherwise.
+template <typename TType>
+constexpr bool inRange(TType check, TType min, TType max) noexcept
+{
+    return min <= check && check <= max;
+}
+
+/// @brief Checks if the given check is in the given range.
+///
+/// @tparam TRangeType The type of the min and max.
+/// @tparam TCheckType The type of the check.
+/// @param check The value to check.
+/// @param min The value smaller or equal to check.
+/// @param max The value greater or equal to check.
+/// @return True if check is within the range, false otherwise.
+template <typename TRangeType, typename TCheckType>
+constexpr bool inRange(TCheckType check, TRangeType min, TRangeType max) noexcept
+{
+    static_assert(std::is_pointer_v<TRangeType> == std::is_pointer_v<TCheckType>,
+                  "inRange function called using pointer and non pointer type.");
+    return min <= check && check <= max;
+}
+
+} // namespace Terrahertz
+
+#endif // !THZ_COMMON_UTILITY_INRANGE_H
---
+++ Tests/Math/InRange.cpp
@@ -0,0 +1,38 @@
+#include "Math/InRange.h"
+
+#include <gtest/gtest.h>
+
+namespace Terrahertz::UnitTests {
+
+struct Math_InRange : public testing::Test
+{};
+
+TEST_F(Math_InRange, ValidRangeChecksOneType)
+{
+    EXPECT_FALSE(inRange(-11, -10, 10));
+    EXPECT_TRUE(inRange(-10, -10, 10));
+    EXPECT_TRUE(inRange(0, -10, 10));
+    EXPECT_TRUE(inRange(10, -10, 10));
+    EXPECT_FALSE(inRange(11, -10, 10));
+}
+
+TEST_F(Math_InRange, ValidRangeChecksMultipleTypes)
+{
+    EXPECT_FALSE(inRange(-11.0, -10, 10));
+    EXPECT_TRUE(inRange(-10.0, -10, 10));
+    EXPECT_TRUE(inRange(0.0, -10, 10));
+    EXPECT_TRUE(inRange(10.0, -10, 10));
+    EXPECT_FALSE(inRange(11.0, -10, 10));
+}
+
+TEST_F(Math_InRange, SwappingMinGreaterMaxReturnsFalse)
+{
+    EXPECT_FALSE(inRange(-11.0, 10, -10));
+    EXPECT_FALSE(inRange(0.0, 10, -10));
+    EXPECT_FALSE(inRange(11.0, 10, -10));
+    EXPECT_FALSE(inRange(-11, 10, -10));
+    EXPECT_FALSE(inRange(0, 10, -10));
+    EXPECT_FALSE(inRange(11, 10, -10));
+}
+
+} // namespace Terrahertz::UnitTests
