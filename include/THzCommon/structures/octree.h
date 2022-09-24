9d70be921f1de9cc67c3fc424c058dec21f4788f
Thomas Schlott <thomas.schlott@outlook.de>
2022-09-25 00:06:38 +0200

    Octree: Added class for working with octrees

---
+++ include/THzCommon/structures/octree.h
@@ -0,0 +1,359 @@
+#ifndef THZ_COMMON_STRUCTURES_OCTREE_H
+#define THZ_COMMON_STRUCTURES_OCTREE_H
+
+#include <cstdint>
+#include <gsl/span>
+#include <type_traits>
+#include <vector>
+
+namespace Terrahertz {
+
+/// @brief Implementation of an octree based on a cube shaped space.
+///
+/// @tparam TCoordinateType The type used for the coordinates inside the octree space.
+/// @tparam TSizeType The type used to measure the octree space (needs to be bigger than TCoordinateType).
+/// @tparam TDataType The data type of the elements inside the octree.
+template <typename TCoordinateType, typename TSizeType, typename TDataType>
+class Octree
+{
+public:
+    static_assert(std::is_arithmetic_v<TCoordinateType>, "TCoordinateType needs to be arithmetic");
+    static_assert(sizeof(TCoordinateType) <= sizeof(TSizeType),
+                  "TCoordinateType needs to be smaller or equal to TSizeType");
+
+    /// @brief Represents an entry in the octree.
+    struct Entry
+    {
+        /// @brief The x coordinate of the entry.
+        TCoordinateType x{};
+
+        /// @brief The y coordinate of the entry.
+        TCoordinateType y{};
+
+        /// @brief The z coordinate of the entry.
+        TCoordinateType z{};
+
+        /// @brief The value of the entry.
+        TDataType value{};
+    };
+
+    /// @brief Shortcut to this type.
+    using MyType = Octree<TCoordinateType, TSizeType, TDataType>;
+
+    /// @brief A vector of pointers to constant nodes on a Octree.
+    using CNodePointerVector = std::vector<MyType const *>;
+
+    /// @brief Default initializes a new Octree.
+    Octree() noexcept = default;
+
+    /// @brief Initializes a new Octree at the given position.
+    ///
+    /// @param centerX The geometric center of the node on the x-axis.
+    /// @param centerY The geometric center of the node on the y-axis.
+    /// @param centerZ The geometric center of the node on the z-axis.
+    /// @param size The size of the node in all directions.
+    /// @param maxEntries The maximum number of entries in a node before it gets subdivided.
+    /// @param maxDepth The maximum depth of the tree.
+    Octree(TCoordinateType centerX,
+           TCoordinateType centerY,
+           TCoordinateType centerZ,
+           TSizeType       size,
+           uint32_t        maxEntries,
+           uint8_t         maxDepth) noexcept
+        : _centerX{centerX},
+          _centerY{centerY},
+          _centerZ{centerZ},
+          _size{size},
+          _maxEntries{maxEntries},
+          _maxDepth{maxDepth}
+    {}
+
+    /// @brief Returns the center of the octree on the x axis.
+    ///
+    /// @return The center of the octree on the x axis.
+    TCoordinateType getCenterX() const noexcept { return _centerX; }
+
+    /// @brief Returns the center of the octree on the y axis.
+    ///
+    /// @return The center of the octree on the y axis.
+    TCoordinateType getCenterY() const noexcept { return _centerY; }
+
+    /// @brief Returns the center of the octree on the z axis.
+    ///
+    /// @return The center of the octree on the z axis.
+    TCoordinateType getCenterZ() const noexcept { return _centerZ; }
+
+    /// @brief Returns the size of the octree.
+    ///
+    /// @return The size of the octree.
+    TSizeType getSize() const noexcept { return _size; }
+
+    /// @brief Returns the maximum number of entries in a node before it gets subdivided.
+    ///
+    /// @return The maximum number of entries in a node before it gets subdivided.
+    uint32_t getMaxEntries() const noexcept { return _maxEntries; }
+
+    /// @brief Return the maximum depth of the tree.
+    ///
+    /// @return The maximum depth of the tree.
+    uint8_t getMaxDepth() const noexcept { return _maxDepth; }
+
+    /// @brief Adds a new entry to the octree.
+    ///
+    /// @param e The entry to add.
+    void addEntry(Entry const &e) noexcept
+    {
+        _touched = true;
+        if (!_children.empty())
+        {
+            auto index = 0U;
+            if (e.x >= _centerX)
+            {
+                index |= 0x1;
+            }
+            if (e.y >= _centerY)
+            {
+                index |= 0x2;
+            }
+            if (e.z >= _centerZ)
+            {
+                index |= 0x4;
+            }
+            _children[index].addEntry(e);
+        }
+        else
+        {
+            if ((_entries.size() < _maxEntries) || (_maxDepth == 0))
+            {
+                _entries.push_back(e);
+            }
+            else
+            {
+                // split node
+                auto const childSize   = _size / 2;
+                auto const childOffset = childSize / 2;
+
+                auto const xp = _centerX + childOffset;
+                auto const xn = _centerX - childOffset;
+                auto const yp = _centerY + childOffset;
+                auto const yn = _centerY - childOffset;
+                auto const zp = _centerZ + childOffset;
+                auto const zn = _centerZ - childOffset;
+                _children.emplace_back(xn, yn, zn, childSize, _maxEntries, _maxDepth - 1);
+                _children.emplace_back(xp, yn, zn, childSize, _maxEntries, _maxDepth - 1);
+                _children.emplace_back(xn, yp, zn, childSize, _maxEntries, _maxDepth - 1);
+                _children.emplace_back(xp, yp, zn, childSize, _maxEntries, _maxDepth - 1);
+                _children.emplace_back(xn, yn, zp, childSize, _maxEntries, _maxDepth - 1);
+                _children.emplace_back(xp, yn, zp, childSize, _maxEntries, _maxDepth - 1);
+                _children.emplace_back(xn, yp, zp, childSize, _maxEntries, _maxDepth - 1);
+                _children.emplace_back(xp, yp, zp, childSize, _maxEntries, _maxDepth - 1);
+
+                addEntry(e);
+                for (auto const &entry : _entries)
+                {
+                    addEntry(entry);
+                }
+                _entries.clear();
+            }
+        }
+    }
+
+    /// @brief Initializes a new Octree at the given position.
+    ///
+    /// @param x The x coordinate of the entry.
+    /// @param y The y coordinate of the entry.
+    /// @param z The z coordinate of the entry.
+    /// @param value The value coordinate of the entry.
+    void addEntry(TCoordinateType x, TCoordinateType y, TCoordinateType z, TDataType value) noexcept
+    {
+        addEntry(Entry{x, y, z, value});
+    }
+
+    /// @brief Resets this node, removing all children and entries and setting the given values.
+    ///
+    /// @param centerX The geometric center of the node on the x-axis.
+    /// @param centerY The geometric center of the node on the y-axis.
+    /// @param centerZ The geometric center of the node on the z-axis.
+    /// @param size The size of the node in all directions.
+    /// @param maxEntries The maximum number of entries in a node before it gets subdivided.
+    /// @param maxDepth The maximum depth of the tree.
+    void reset(TCoordinateType centerX,
+               TCoordinateType centerY,
+               TCoordinateType centerZ,
+               TSizeType       size,
+               uint32_t        maxEntries,
+               uint8_t         maxDepth) noexcept
+    {
+        _centerX    = centerX;
+        _centerY    = centerY;
+        _centerZ    = centerZ;
+        _size       = size;
+        _maxEntries = maxEntries;
+        _maxDepth   = maxDepth;
+        reset();
+    }
+
+    /// @brief Resets this node, removing all children and entries.
+    void reset() noexcept
+    {
+        if (!_children.empty())
+        {
+            for (auto &child : _children)
+            {
+                child.reset();
+            }
+            _children.clear();
+        }
+        _entries.clear();
+    }
+
+    /// @brief Returns the children of this node, or an empty span if this nodes has no children.
+    ///
+    /// @return The children of this node, or an empty span if this nodes has no children.
+    gsl::span<Octree const> getChildNodes() const noexcept { return {_children}; }
+
+    /// @brief Returns pointers to all nodes inside the Octree.
+    ///
+    /// @param nodes Output: A vector to store the pointers to all nodes inside the Octree.
+    void getNodes(std::vector<MyType const *> &nodes) const noexcept
+    {
+        nodes.push_back(this);
+        for (auto const &child : _children)
+        {
+            child.getNodes(nodes);
+        }
+    }
+
+    /// @brief Returns pointers to all nodes inside the Octree.
+    ///
+    /// @param nodes Output: A vector to store the pointers to all nodes inside the Octree.
+    /// @param layer The layer from which the nodes shall be retrieved, 0 for this layer, 1 for child layer and so on.
+    void getNodes(std::vector<MyType const *> &nodes, size_t const layer) const noexcept
+    {
+        if (layer == 0)
+        {
+            nodes.push_back(this);
+        }
+        else
+        {
+            for (auto const &child : _children)
+            {
+                child.getNodes(nodes, layer - 1);
+            }
+        }
+    }
+
+    /// @brief Returns a vector of pointers to all nodes inside the Octree.
+    ///
+    /// @return A vector of pointers to all nodes inside the Octree.
+    CNodePointerVector getNodes() const noexcept
+    {
+        CNodePointerVector nodes{};
+        getNodes(nodes);
+        return nodes;
+    }
+
+    /// @brief Returns a vector of pointers to all nodes inside the Octree.
+    ///
+    /// @param layer The layer from which the nodes shall be retrieved, 0 for this layer, 1 for child layer and so on.
+    /// @return A vector of pointers to all nodes inside the Octree.
+    CNodePointerVector getNodes(size_t const layer) const noexcept
+    {
+        CNodePointerVector nodes{};
+        getNodes(nodes, layer);
+        return nodes;
+    }
+
+    /// @brief Uses the given function to analyze the entries of this node and its children.
+    ///
+    /// @tparam TFunction The type of the function to analyze the entries.
+    /// @param function The function to analyze the entries.
+    template <typename TFunction>
+    void analyzeEntries(TFunction &function) const noexcept
+    {
+        for (auto const &entry : _entries)
+        {
+            function(entry);
+        }
+        for (auto const &child : _children)
+        {
+            child.analyzeEntries(function);
+        }
+    }
+
+    /// @brief Recalculates all meta data for faster processing.
+    void recalculate() noexcept
+    {
+        if (_touched)
+        {
+            _totalEntries = _entries.size();
+            for (auto &child : _children)
+            {
+                child.recalculate();
+                _totalEntries += child.totalEntries();
+            }
+            _touched = false;
+        }
+    }
+
+    /// @brief Returns the number of entries this node or its children have in total.
+    ///
+    /// @return The number of entries this node or its children have in total.
+    size_t totalEntries() noexcept
+    {
+        recalculate();
+        return _totalEntries;
+    }
+
+    /// @brief Returns the number of entries this node or its children have in total.
+    ///
+    /// @return The number of entries this node or its children have in total.
+    size_t totalEntries() const noexcept
+    {
+        if (_touched)
+        {
+            auto totalEntries = _entries.size();
+            for (auto &child : _children)
+            {
+                totalEntries += child.totalEntries();
+            }
+            return totalEntries;
+        }
+        return _totalEntries;
+    }
+
+private:
+    /// @brief The geometric center of the node on the x-axis.
+    TCoordinateType _centerX{};
+
+    /// @brief The geometric center of the node on the y-axis.
+    TCoordinateType _centerY{};
+
+    /// @brief The geometric center of the node on the z-axis.
+    TCoordinateType _centerZ{};
+
+    /// @brief The size of the node in all directions.
+    TSizeType _size{};
+
+    /// @brief The maximum number of entries in a node before it gets subdivided.
+    uint32_t _maxEntries{};
+
+    /// @brief The maximum depth of the tree.
+    uint8_t _maxDepth{};
+
+    /// @brief The child nodes of this node.
+    std::vector<Octree> _children{};
+
+    /// @brief The entries of this node.
+    std::vector<Entry> _entries{};
+
+    /// @brief Flag signalling if the node has been changed.
+    bool _touched{};
+
+    /// @brief The total number of entries in this node or its children.
+    size_t _totalEntries{};
+};
+
+} // namespace Terrahertz
+
+#endif // !THZ_COMMON_STRUCTURES_OCTREE_H
---
+++ test/structures/octree.cpp
@@ -0,0 +1,135 @@
+#include "THzCommon/structures/octree.h"
+
+#include <cstdint>
+#include <gtest/gtest.h>
+#include <random>
+
+namespace Terrahertz::UnitTests {
+
+struct Structures_Octree : public testing::Test
+{
+    using TestOctreeType = Octree<std::uint16_t, std::uint32_t, std::int32_t>;
+
+    TestOctreeType sut{128U, 128U, 128U, 256U, 4U, 8U};
+
+    void checkTree(TestOctreeType const &tree,
+                   std::uint16_t const   centerX,
+                   std::uint16_t const   centerY,
+                   std::uint16_t const   centerZ,
+                   std::uint32_t const   size,
+                   std::uint32_t const   maxEntries,
+                   std::uint8_t const    maxDepth,
+                   size_t const          totalEntries) noexcept
+    {
+
+        EXPECT_EQ(tree.getCenterX(), centerX);
+        EXPECT_EQ(tree.getCenterY(), centerY);
+        EXPECT_EQ(tree.getCenterZ(), centerZ);
+        EXPECT_EQ(tree.getSize(), size);
+        EXPECT_EQ(tree.getMaxEntries(), maxEntries);
+        EXPECT_EQ(tree.getMaxDepth(), maxDepth);
+        EXPECT_EQ(tree.totalEntries(), totalEntries);
+    }
+};
+
+TEST_F(Structures_Octree, DefaultConstruction)
+{
+    TestOctreeType tree{};
+    checkTree(tree, {}, {}, {}, {}, {}, {}, 0U);
+    EXPECT_TRUE(tree.getChildNodes().empty());
+    auto const nodes = tree.getNodes();
+    EXPECT_EQ(nodes.size(), 1U);
+    for (auto i = 0U; i < 4U; ++i)
+    {
+        auto const nodesLayer = tree.getNodes(i);
+        EXPECT_EQ(nodesLayer.size(), (i == 0 ? 1U : 0U));
+    }
+}
+
+TEST_F(Structures_Octree, ParameterConstruction)
+{
+    TestOctreeType tree{127U, 128U, 126U, 255U, 4U, 8U};
+    checkTree(tree, 127U, 128U, 126U, 255U, 4U, 8U, 0U);
+    EXPECT_TRUE(tree.getChildNodes().empty());
+    auto const nodes = tree.getNodes();
+    EXPECT_EQ(nodes.size(), 1U);
+    for (auto i = 0U; i < 4U; ++i)
+    {
+        auto const nodesLayer = tree.getNodes(i);
+        EXPECT_EQ(nodesLayer.size(), (i == 0 ? 1U : 0U));
+    }
+}
+
+TEST_F(Structures_Octree, AddSingleEntry)
+{
+    sut.addEntry(20U, 20U, 20U, 123U);
+    EXPECT_EQ(sut.totalEntries(), 1U);
+    EXPECT_TRUE(sut.getChildNodes().empty());
+    auto const nodes = sut.getNodes();
+    EXPECT_EQ(nodes.size(), 1U);
+    for (auto i = 0U; i < 4U; ++i)
+    {
+        auto const nodesLayer = sut.getNodes(i);
+        EXPECT_EQ(nodesLayer.size(), (i == 0 ? 1U : 0U));
+    }
+}
+
+TEST_F(Structures_Octree, NodeIsSplitCorrectly)
+{
+    for (auto i = 0U; i < 4U; ++i)
+    {
+        sut.addEntry(40U * i, 40U * i, 40U * i, 123U);
+    }
+    auto nodes = sut.getNodes();
+    EXPECT_EQ(nodes.size(), 1U);
+    sut.addEntry(160U, 160U, 160U, 123U);
+    nodes = sut.getNodes();
+    EXPECT_EQ(nodes.size(), 9U);
+
+    // check other nodes
+    checkTree(*nodes[0], 128U, 128U, 128U, 256U, 4U, 8U, 5U);
+    checkTree(*nodes[1], 64U, 64U, 64U, 128U, 4U, 7U, 4U);
+    checkTree(*nodes[2], 192U, 64U, 64U, 128U, 4U, 7U, 0U);
+    checkTree(*nodes[3], 64U, 192U, 64U, 128U, 4U, 7U, 0U);
+    checkTree(*nodes[4], 192U, 192U, 64U, 128U, 4U, 7U, 0U);
+    checkTree(*nodes[5], 64U, 64U, 192U, 128U, 4U, 7U, 0U);
+    checkTree(*nodes[6], 192U, 64U, 192U, 128U, 4U, 7U, 0U);
+    checkTree(*nodes[7], 64U, 192U, 192U, 128U, 4U, 7U, 0U);
+    checkTree(*nodes[8], 192U, 192U, 192U, 128U, 4U, 7U, 1U);
+}
+
+TEST_F(Structures_Octree, Reset)
+{
+    {
+        TestOctreeType tree{};
+        tree.addEntry(20U, 20U, 20U, 20U);
+        tree.reset(12U, 13U, 14U, 24U, 4U, 3U);
+        checkTree(tree, 12U, 13U, 14U, 24U, 4U, 3U, 0U);
+    }
+    {
+        TestOctreeType tree{12U, 13U, 14U, 24U, 4U, 3U};
+        tree.addEntry(20U, 20U, 20U, 20U);
+        tree.reset(14U, 11U, 14U, 48U, 8U, 7U);
+        checkTree(tree, 14U, 11U, 14U, 48U, 8U, 7U, 0U);
+    }
+}
+
+TEST_F(Structures_Octree, AnalyzeEntries)
+{
+    for (auto i = 20U; i < 80; ++i)
+    {
+        sut.addEntry(i, i, i, i);
+    }
+
+    auto count  = 0U;
+    auto lambda = [&](TestOctreeType::Entry const &e) {
+        EXPECT_EQ(e.x, e.y);
+        EXPECT_EQ(e.x, e.z);
+        EXPECT_EQ(e.x, e.value);
+        ++count;
+    };
+    sut.analyzeEntries(lambda);
+    EXPECT_EQ(count, sut.totalEntries());
+}
+
+} // namespace Terrahertz::UnitTests
