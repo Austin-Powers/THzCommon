c20f5dddbe97db9a0abce09fe49a582161c89857
Thomas Schlott <thomas.schlott@outlook.de>
2022-12-15 15:53:05 +0100

    flipBuffer: Added template function to flipp a buffer containing 2D data around the x-axis

---
+++ include/THzCommon/utility/flipBuffer.h
@@ -0,0 +1,43 @@
+#ifndef THZ_COMMON_UTILITY_FLIPBUFFER_H
+#define THZ_COMMON_UTILITY_FLIPBUFFER_H
+
+#include "THzCommon/math/rectangle.h"
+
+#include <gsl/gsl>
+
+namespace Terrahertz {
+
+/// @brief Flips the given buffer along the X-axis.
+///
+/// @tparam TValueType The value type of the buffer.
+/// @param buffer The buffer to flip.
+/// @param dimensions The dimensions of the buffer.
+/// @return False in case dimensions.area() does not match buffer.size(), true otherwise.
+template <typename TValueType>
+bool flipBufferHorizontally(gsl::span<TValueType> buffer, Rectangle const &dimensions) noexcept
+{
+    if (buffer.size() != dimensions.area())
+    {
+        return false;
+    }
+
+    TValueType temp{};
+    for (auto y = 0U; y < (dimensions.height / 2U); ++y)
+    {
+        auto upperRow = buffer.data() + (dimensions.width * y);
+        auto lowerRow = buffer.data() + (dimensions.width * (dimensions.height - y - 1U));
+        for (auto x = 0U; x < dimensions.width; ++x)
+        {
+            temp        = (*upperRow);
+            (*upperRow) = (*lowerRow);
+            (*lowerRow) = temp;
+            ++upperRow;
+            ++lowerRow;
+        }
+    }
+    return true;
+}
+
+} // namespace Terrahertz
+
+#endif // !THZ_COMMON_UTILITY_FLIPBUFFER_H
---
+++ test/utility/flipBuffer.cpp
@@ -0,0 +1,85 @@
+#include "THzCommon/utility/flipBuffer.h"
+
+#include "THzCommon/math/rectangle.h"
+#include "THzCommon/utility/spanhelpers.h"
+
+#include <array>
+#include <cstdint>
+#include <gtest/gtest.h>
+
+namespace Terrahertz::UnitTests {
+
+struct Utility_FlipBuffer : public testing::Test
+{};
+
+TEST_F(Utility_FlipBuffer, BufferDimensionsMismatch)
+{
+    std::array<std::int32_t, 10U> buffer{};
+    Rectangle const               dimensions{5U, 5U};
+    EXPECT_FALSE(flipBufferHorizontally(toSpan<std::int32_t>(buffer), dimensions));
+}
+
+TEST_F(Utility_FlipBuffer, FlipHorizontallyWithEvenNumberOfRows)
+{
+    // 01 02 03 04    13 14 15 16
+    // 05 06 07 08 -> 09 10 11 12
+    // 09 10 11 12    05 06 07 08
+    // 13 14 15 16    01 02 03 04
+
+    std::array<std::int32_t, 16U> buffer{};
+
+    std::int32_t i = 0;
+    for (auto &entry : buffer)
+    {
+        entry = ++i;
+    }
+    Rectangle const dimensions{4U, 4U};
+    EXPECT_TRUE(flipBufferHorizontally(toSpan<std::int32_t>(buffer), dimensions));
+    i      = 13;
+    auto x = 0;
+    for (auto const &entry : buffer)
+    {
+        ASSERT_EQ(entry, i);
+        ++i;
+        ++x;
+        if (x == 4)
+        {
+            x = 0;
+            i -= 8;
+        }
+    }
+}
+
+TEST_F(Utility_FlipBuffer, FlipHorizontallyWithOddNumberOfRows)
+{
+    // 01 02 03 04 05    21 22 23 24 25
+    // 06 07 08 09 10    16 17 18 19 20
+    // 11 12 13 14 15 -> 11 12 13 14 15
+    // 16 17 18 19 20    06 07 08 09 10
+    // 21 22 23 24 25    01 02 03 04 05
+
+    std::array<std::int32_t, 25U> buffer{};
+
+    std::int32_t i = 0;
+    for (auto &entry : buffer)
+    {
+        entry = ++i;
+    }
+    Rectangle const dimensions{5U, 5U};
+    EXPECT_TRUE(flipBufferHorizontally(toSpan<std::int32_t>(buffer), dimensions));
+    i      = 21;
+    auto x = 0;
+    for (auto const &entry : buffer)
+    {
+        ASSERT_EQ(entry, i);
+        ++i;
+        ++x;
+        if (x == 5)
+        {
+            x = 0;
+            i -= 10;
+        }
+    }
+}
+
+} // namespace Terrahertz::UnitTests
